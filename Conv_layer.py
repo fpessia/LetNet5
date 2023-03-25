993369e77a113aceafff62eeebad03864cd9cb82
fpessia <fpessia@libero.it>
2023-03-25 10:24:54 +0100

    Finished conv layer

--- Conv_layer.py
+++ Conv_layer.py
@@ -10,42 +10,80 @@ class Conv_layer():
         self.w = torch.randn(number_of_filters,n_channels,filter_size,filter_size)
         self.b = torch.randn(1,number_of_filters)
         self.output_size = (input_size- filter_size) + 1
+        self.last_input = torch.empty(n_channels,input_size,input_size)
 
-    def forward(x):
+    def forward(self,x):
         for f in range(self.number_of_filters):
             for i in range(self.output_size):
                     for j in range(self.output_size):
                         y[f][i][j] = 0
                         for c in range(self.n_channels):
                             for k in range(self.filter_size):
-                                for l in range(self.output_size):
+                                for l in range(self.filter_size):
                                     y[f][i][j] += self.w[f][c][k][l] * x[c][i+k][j+l]
                         y[f][i][j] += self.b[1][f]
+        self.last_input = x
         return y
     
-    def back_prop(dy):
+    def back_prop(self,dy):
         # As a fisrt step I calculate db, dw, dx
         #then I update w & b
-        for f in range(number_of_filters):
-            db[f]= 0
-            for i in range(output_size):
-                for j in range(output_size):
-                    db[f] += dy[f][i][j]
-        #Now i proced to find dw
+        db = torch.empty(1,self.number_of_filters)
+        dw = torch.empty(self.number_of_filters,self.n_channels,self.filter_size,self.filter_size)
+        dx = torch.zeros(self.n_channels,self.input_size+2,self.input_size+2) #already zero padded
 
-        for f in range(number_of_filters):
-            for c in range(n_channels):
-                for k in range(output_size):
-                    for l in range(output_size):
+
+        for f in range(self.number_of_filters):
+            db[f]= 0
+            for i in range(self.output_size):
+                for j in range(self.output_size):
+                    db[1,f] += dy[f][i][j]
+       
+        #Now i proced to find dw
+        for f in range(self.number_of_filters):
+            for c in range(self.n_channels):
+                for k in range(self.filter_size):
+                    for l in range(self.filter_size):
                         dw[f][c][k][l] = 0 
-                        for i in range(output_size):
-                            for j in range(output_size):
-                                for c_n in range(n_channels):
-                                    dw[f][c][k][l] += 
+                        for i in range(self.output_size):
+                            for j in range(self.output_size):
+                                #for c_n in range(self.n_channels):
+                                dw[f][c][k][l] += dy[f][i][j] * self.last_input[c][i+k][j+l]
+        
+        #Now I procede to find dx = dy_0 * w'
+        # I need zero padding of dy & manually transpose w
+        dy_0 = torch.zeros(self.number_of_filters,self.output_size+2, self.output_size+2)
+        for f in range(self.number_of_filters):
+            for w in  range(self.output_size):
+                for h in range(self.output_size):
+                    dy_0[f][w+1][h+1] = dy[f][w][h] 
+     
+        #transposing w
+        w_transposed = torch.zeros(self.number_of_filters,self.n_channels,self.filter_size,self.filter_size)
+        for i in range(self.filter_size):
+            for j in range(self.filter_size):
+                w_transposed[:,:,i,j] = self.w[:,:,self.filter_size-i-1,self.filter_size-j-1]
+
+            
+        for f in range(self.number_of_filters):   
+            for i in range(self.input_size+2): 
+                for j in range(self.input_size+2):
+                    for k in range(self.filter_size): 
+                        for l in range(self.filter_size):
+                            for c in range(self.n_channels): 
+                                dx[c,i,j] += dy_0[f, i+k, j+l] * w_transposed[f, c, k, l]
         
     
-
+        #Now I update w & b according to learinig rate
+        for f in range(self.number_of_filters):
+            self.b[1][f] -= self.learning_rate * db[1][f]
+        
+        for f in range(self.number_of_filters):
+            for c in range(self.n_channels):
+                for i in range(self.filter_size):
+                    for j in range(self.filter_size):
+                        self.w[f][c][i][j] -= self.learning_rate * dw[f][c][i][j]
     
-
-
+        
+        return dx[:,1:-1,1:-1]
 
--- dummy_script.py
+++ dummy_script.py
@@ -1,4 +1,10 @@
 import torch
 
-for i in range(1, 11):
-    print(i)
+
+x = torch.empty(2,2)
+print(x.size())
+
+x[1][1] = 1
+for i in range(2):
+    for j in range(2):
+        print(x[i][j])

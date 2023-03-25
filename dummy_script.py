43c6394f80273c7e5dd960cc0580832f6e8b36d7
fpessia <fpessia@libero.it>
2023-03-25 14:36:22 +0100

    Activation_layer_in_dummy

--- dummy_script.py
+++ dummy_script.py
@@ -1,17 +1,26 @@
 import torch
 import numpy as np
+import sys
 
 
-x1 = [[2,2],[2,2],[2,2]]
+#x = torch.tensor([[2.0,2.0],[2.0,2.0],[2.0,2.0]],requires_grad=True)
 #print(x1.size())
 
-x = np.ones_like(x1)
-x = np.pad(x,((0,),(1,),(1,)),'constant')
 
+#x = torch.randn(3, requires_grad = True)
+x = torch.zeros(2,2, requires_grad = True)
 print(x)
-for i in range(4):
-    for j in range(4):
-        print(x[i][j])
+x = torch.tensor([[0.9, -0.67],[0.7, 0.4]], requires_grad = True)
+print(x)
+y = torch.tanh(x)
+print(y)
+dy = torch.tensor([[0.01, 0.0001],[ 0.1, 1.0]])
+y.backward(dy)
+print(x.grad)
+
+
+sys.exit()
+
 
 
         #  for f in range(self.number_of_filters):   
@@ -25,14 +34,16 @@ for i in range(4):
 
 
  # I need zero padding of dy & manually transpose w
-        dy_0 = torch.zeros(self.number_of_filters,self.output_size+2, self.output_size+2)
-        for f in range(self.number_of_filters):
-            for w in  range(self.output_size):
-                for h in range(self.output_size):
-                    dy_0[f][w+1][h+1] = dy[f][w][h] 
+
+ #
+  #      dy_0 = torch.zeros(self.number_of_filters,self.output_size+2, self.output_size+2)
+   #     for f in range(self.number_of_filters):
+   #         for w in  range(self.output_size):
+    #            for h in range(self.output_size):
+     #               dy_0[f][w+1][h+1] = dy[f][w][h] 
      
         #transposing w
-        w_transposed = torch.zeros(self.number_of_filters,self.n_channels,self.filter_size,self.filter_size)
-        for i in range(self.filter_size):
-            for j in range(self.filter_size):
-                w_transposed[:,:,i,j] = self.w[:,:,self.filter_size-i-1,self.filter_size-j-1]
+      #  w_transposed = torch.zeros(self.number_of_filters,self.n_channels,self.filter_size,self.filter_size)
+       # for i in range(self.filter_size):
+        #    for j in range(self.filter_size):
+         #       w_transposed[:,:,i,j] = self.w[:,:,self.filter_size-i-1,self.filter_size-j-1]

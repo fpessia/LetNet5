Fixed vanishing gradient

Conv_layer.py
@@ -1,4 +1,5 @@
 import torch
+from math import sqrt
 from pathos.multiprocessing import ProcessingPool as Pool
 
 class Conv_layer():
@@ -8,9 +9,14 @@ class Conv_layer():
         self.number_of_filters = number_of_filters
         self.filter_size = filter_size
         self.learning_rate = learning_rate
-        self.w = torch.randn(number_of_filters,n_channels,filter_size,filter_size)
-        self.b = torch.randn(1,number_of_filters)
         self.output_size = (input_size- filter_size) + 1
+
+        self.len_prev_layer = self.input_size * self.input_size * self.n_channels
+        self.len_layer_out = self.number_of_filters * self.output_size * self.output_size
+
+        self.w = torch.randn(number_of_filters,n_channels,filter_size,filter_size) * sqrt(2/(self.len_prev_layer + self.len_layer_out))
+        self.b = torch.zeros(1,number_of_filters)
+        
         self.last_input = torch.empty(n_channels,input_size,input_size)
         self.map = Pool().map
         self.y = torch.zeros(self.number_of_filters,self.output_size,self.output_size)

Fully_connected_layer.py
@@ -1,18 +1,19 @@
 import torch
 from pathos.multiprocessing import ProcessingPool as Pool
+from math import sqrt
 
 class Fully_connected_layer():
     def __init__(self,input_size,output_size,learning_rate):
         self.input_size = input_size
         self.output_size = output_size
         self.learning_rate = learning_rate
-        self.w = torch.randn(self.output_size,self.input_size)
-        self.b = torch.randn(1,self.output_size)
+        self.w = torch.randn(self.output_size,self.input_size)* sqrt(2/(self.input_size + self.output_size))
+        self.b = torch.zeros(1,self.output_size)
         self.last_input = torch.zeros(input_size)
         self.y = torch.zeros(self.output_size)
         self.map = Pool().map
-        self.dw = torch.zeros(self.output_size,self.input_size)
-        self.db = torch.zeros(1,self.output_size)
+        self.dw = torch.zeros(self.output_size,self.input_size) 
+        self.db = torch.zeros(1,self.output_size) 
         self.dx = torch.zeros(self.input_size)
         self.dy = torch.zeros(self.output_size)
         
@@ -57,6 +58,7 @@ class Fully_connected_layer():
                 self.w[n] = updated_w_list[n]
                 self.b[0][n] -= self.learning_rate * self.db[0][n]
 
+        
         return self.dx
     
 